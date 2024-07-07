aec20df0c63d02bc910bd99d2db555a00064166a
DantonSoftware <80655836+DantonSoftware@users.noreply.github.com>
2024-07-07 19:20:59 -0400

    Login y entidades

---
+++ CapaPresentacion/FrmLogin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            FrmInicio frm = new FrmInicio();
+            frm.Show();
+            this.Hide();
+
+            frm.FormClosing += frm_closing;
+        }
+
+        private void frm_closing(object sender, FormClosingEventArgs e)
+        {
+            txtUsuario.Text = "";
+            txtClave.Text = "";
+            this.Show();
+        }
+    }
+}
